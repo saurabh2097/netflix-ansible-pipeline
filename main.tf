fe2d1d99041f72eb086bf47c1b16ef60b205cb78
saurabh2097 <saurabhchimankar97@gmail.com>
2025-04-07 19:13:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,5 +14,5 @@ Name = var.instance_names[count.index]
 }
 
 variable "instance_names" {
-default = ["jenkins & ansible", "dev-1", "dev-2","test-1","test-2" "Monitoring server"]
+default = ["jenkins & ansible", "dev-1", "dev-2","test-1","test-2" ,"Monitoring server"]
 }
